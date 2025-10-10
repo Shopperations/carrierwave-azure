dce2571a20a5e6f6333df88c72205acfeaae3142
ArtZlab <artem.zaslavskiy@gmail.com>
2025-10-10 19:18:06 +0300

    Add config fixes

--- lib/carrierwave-azure.rb
+++ lib/carrierwave-azure.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'carrierwave'
-require 'azure/storage/blob'
+require 'azure_blob'
 require 'carrierwave/storage/azure'
 
 class CarrierWave::Uploader::Base
@@ -7,6 +9,8 @@ class CarrierWave::Uploader::Base
   add_config :azure_storage_access_key
   add_config :azure_storage_blob_host
   add_config :azure_container
+  add_config :azure_public
+  add_config :azure_url_expires_in
 
   configure do |config|
     config.storage_engines[:azure] = 'CarrierWave::Storage::Azure'
