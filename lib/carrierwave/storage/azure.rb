85ca62022ce20da1d4b25f2d05d30ba6dc769ed4
ArtZlab <artem.zaslavskiy@gmail.com>
2025-10-10 19:07:44 +0300

    Add migration to azure-blob

--- lib/carrierwave/storage/azure.rb
+++ lib/carrierwave/storage/azure.rb
@@ -1,27 +1,21 @@
-require 'azure/storage/blob'
+# frozen_string_literal: true
+
+require "uri"
+require "time"
+require "stringio"
+require "azure_blob"
 
 module CarrierWave
   module Storage
     class Azure < Abstract
       def store!(file)
-        azure_file = CarrierWave::Storage::Azure::File.new(uploader, connection, uploader.store_path)
-        azure_file.store!(file)
-        azure_file
+        f = CarrierWave::Storage::Azure::File.new(uploader, connection, uploader.store_path)
+        f.store!(file)
+        f
       end
 
-      def retrieve!(identifer)
-        CarrierWave::Storage::Azure::File.new(uploader, connection, uploader.store_path(identifer))
-      end
-
-      def connection
-        @connection ||= begin
-          %i[storage_account_name storage_access_key storage_blob_host]
-            .inject({}) do |params, key|
-              param = uploader.public_send("azure_#{key}")
-              param ? params.merge(key => param) : params
-            end
-            .tap { |params| return ::Azure::Storage::Blob::BlobService.create(params) }
-        end
+      def retrieve!(identifier)
+        CarrierWave::Storage::Azure::File.new(uploader, connection, uploader.store_path(identifier))
       end
 
       def cache!(new_file)
@@ -30,98 +24,186 @@ module CarrierWave
         f
       end
 
-      def delete_dir!(path)
-        # do nothing, because there's no such things as 'empty directory'
+      def delete_dir!(_path); end
+
+      def connection
+        @connection ||= Client.new(
+          account_name: uploader.azure_storage_account_name,
+          access_key:   uploader.azure_storage_access_key,
+          container:    uploader.azure_container,
+          host:         (uploader.respond_to?(:azure_storage_blob_host) ? uploader.azure_storage_blob_host : nil)
+        )
+      end
+
+      class Client
+        BlobMeta = Struct.new(:content_type, :size, keyword_init: true)
+
+        def initialize(account_name:, access_key:, container:, host: nil)
+          @account   = account_name
+          @key       = access_key
+          @container = container
+          @host      = host&.to_s&.chomp("/")
+          @client    = AzureBlob::Client.new(
+            account_name: @account,
+            access_key:   @key,
+            container:    @container,
+            host:         @host
+          )
+        end
+
+        def put(key, io, content_type: nil)
+          @client.create_block_blob(key, io, content_type: content_type)
+        end
+
+        def get(key)
+          @client.get_blob(key)
+        end
+
+        def head(key)
+          p = @client.get_blob_properties(key)
+          BlobMeta.new(content_type: p&.content_type, size: p&.content_length || p&.size)
+        end
+
+        def exist?(key)
+          @client.blob_exist?(key)
+        end
+
+        def delete(key)
+          @client.delete_blob(key)
+        end
+
+        def public_url(key)
+          raise "host not configured" unless @host
+          "#{@host}/#{@container}/#{key}"
+        end
+
+        def signed_url(key, permissions:, expiry:)
+          @client.signed_uri(key, permissions: permissions, expiry: expiry).to_s
+        end
       end
 
       class File
         attr_reader :path
 
         def initialize(uploader, connection, path)
-          @uploader = uploader
+          @uploader   = uploader
           @connection = connection
-          @path = path
+          @path       = path
+          @meta       = nil
+          @content    = nil
+          @ctype      = nil
+          @sas_cache  = {}
         end
 
         def store!(file)
-          @content = file.read
-          @content_type = file.content_type
-          @connection.create_block_blob @uploader.azure_container, @path, @content, content_type: @content_type
+          io =
+            if file.respond_to?(:to_io) && file.to_io
+              file.to_io
+            elsif file.respond_to?(:file) && file.file.respond_to?(:to_io)
+              file.file.to_io
+            elsif file.respond_to?(:tempfile) && file.tempfile.respond_to?(:to_io)
+              file.tempfile.to_io
+            else
+              data = file.respond_to?(:read) ? file.read.to_s : file.to_s
+              StringIO.new(data)
+            end
+          @ctype = file.content_type if file.respond_to?(:content_type)
+          @connection.put(@path, io, content_type: @ctype)
           true
         end
 
         def url(options = {})
-          path = ::File.join @uploader.azure_container, @path
-
           if @uploader.asset_host
-            "#{@uploader.asset_host}/#{path}"
-          else
-            "#{@connection.generate_uri(path).to_s}?#{service_sas_token(path)}"
+            path_with_container = ::File.join(@uploader.azure_container, @path)
+            return "#{@uploader.asset_host}/#{path_with_container}"
+          end
+
+          expiry =
+            if options[:expires_at]
+              t = options[:expires_at]
+              t = Time.parse(t.to_s) unless t.is_a?(Time)
+              t.utc
+            else
+              ttl = options[:expires_in] ||
+                    (@uploader.respond_to?(:azure_url_expires_in) && @uploader.azure_url_expires_in) ||
+                    3600
+              Time.now.utc + ttl.to_i
+            end
+
+          perms = options[:permissions] || "r"
+          cache_key = [@path, perms, expiry.to_i]
+
+          @sas_cache[cache_key] ||= begin
+            if public_container?
+              @connection.public_url(@path)
+            else
+              @connection.signed_url(@path, permissions: perms, expiry: expiry)
+            end
           end
         end
 
         def read
-          content
+          load_content_if_needed
+          @content
         end
 
         def content_type
-          @content_type = blob.properties[:content_type] if @content_type.nil? && !blob.nil?
-          @content_type
+          return @ctype if @ctype
+          ensure_meta!
+          @meta&.content_type
         end
 
-        def content_type=(new_content_type)
-          @content_type = new_content_type
+        def content_type=(new_ctype)
+          @ctype = new_ctype
         end
 
         def exitst?
-          blob.nil?
+          !exists?
+        end
+
+        def exists?
+          @connection.exist?(@path)
+        rescue StandardError
+          false
         end
 
         def size
-          blob.properties[:content_length] unless blob.nil?
+          ensure_meta!
+          @meta&.size
         end
 
         def filename
-          URI(url).path.split('/').last
+          URI(url).path.split("/").last
         end
 
         def extension
-          @path.split('.').last
+          @path.split(".").last
         end
 
         def delete
-          begin
-            @connection.delete_blob @uploader.azure_container, @path
-            true
-          rescue ::Azure::Core::Http::HTTPError
-            false
-          end
+          @connection.delete(@path)
+          true
+        rescue StandardError
+          false
         end
 
         private
 
-        def blob
-          load_content if @blob.nil?
-          @blob
+        def ensure_meta!
+          @meta ||= @connection.head(@path)
+        rescue StandardError
+          @meta = nil
         end
 
-        def content
-          load_content if @content.nil?
-          @content
+        def load_content_if_needed
+          return if @content
+          @content = @connection.get(@path)
+        rescue StandardError
+          @content = nil
         end
 
-        def load_content
-          @blob, @content = begin
-            @connection.get_blob @uploader.azure_container, @path
-          rescue ::Azure::Core::Http::HTTPError
-          end
-        end
-
-        def service_sas_token(path)
-          @service_sas_token ||= ::Azure::Storage::Common::Core::Auth::SharedAccessSignature.new(
-            @uploader.azure_storage_account_name,
-            @uploader.azure_storage_access_key
-          ).tap { |generator| return generator.generate_service_sas_token(path, service: 'b') }
+        def public_container?
+          @uploader.respond_to?(:azure_public) && !!@uploader.azure_public
         end
       end
     end
